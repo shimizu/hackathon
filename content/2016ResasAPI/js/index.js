85dfd4dc0d0bbd807aaf5fa7a2915ad88a5aa0f8
SAWA, Norihiko <nakamods320yen@gmail.com>
2016-11-13 13:53:25 +0900

    set back to default by zoom cancel button

--- content/2016ResasAPI/js/index.js
+++ content/2016ResasAPI/js/index.js
@@ -1,52 +1,51 @@
 !(function(){
     var zoom = 2
     var viewPort = [1012, 888]
-    
+
     var t = d3.transition()
         .duration(1000)
 
 
     var stageWidth = document.querySelector(".main__map").clientWidth
     var stageHeight = document.querySelector(".main__map").clientHeight
-    
+
     //console.log("stage", [stageWidth, stageHeight])
-    
+
     var map = d3.select("#main_map")
     var rects = map.selectAll("rect")
-    
+
     //ズーム処理
     rects.on("click", function(){
         rect = d3.select(this)
-        
-        var x = +rect.attr("x") 
+
+        var x = +rect.attr("x")
         var y = +rect.attr("y")
         var w = +rect.attr("width")
         var h = +rect.attr("height")
-        
+
         //console.log("rect", [x,y,w,h])
-        
+
         var nx = (viewPort[0]/2 -(w/2)) / zoom
         var ny = (viewPort[1]/2 -(h/2)) / zoom
-        
+
         rects
         .transition()
         .duration(1000)
         .attr("transform", 'translate('+(0-x)+','+(0-y)+'),translate('+nx+','+ny+')')
-        
+
 
         map
         .transition()
         .duration(1000)
         .attr("transform", "scale("+zoom+")")
     })
-    
+
     //ズーム解除処理
     d3.select("#zoom-off").on("click", function(){
         rects
         .transition()
         .duration(1000)
         .attr("transform", 'translate(0, 0)')
-        
 
         map
         .transition()
@@ -133,7 +132,7 @@ var index = {
             map
             .transition()
             .duration(1000)
-            .attr("transform", "scale(1)")
+            .attr("transform", "translate(0, 250)")
         })
 
     },
